8227182b1396eab308133ac26da2a744b2812085
lakrestofer <lakrestofer@gmail.com>
2025-02-10 14:21:41 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              zola
+              bun
+            ];
+
+          };
+        }
+      );
+    };
+}
